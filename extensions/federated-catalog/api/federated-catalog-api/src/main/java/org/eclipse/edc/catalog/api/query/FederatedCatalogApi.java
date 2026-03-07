eac0bcbbeacb7befda2e0f22af5a462c0bc53c4d
mokhairymahmoud <mokhairy.mahmoud@gmail.com>
2026-03-07 13:39:05 +0100

    refactor the federated catalog API

--- extensions/federated-catalog/api/federated-catalog-api/src/main/java/org/eclipse/edc/catalog/api/query/FederatedCatalogApi.java
+++ extensions/federated-catalog/api/federated-catalog-api/src/main/java/org/eclipse/edc/catalog/api/query/FederatedCatalogApi.java
@@ -29,27 +29,91 @@ import jakarta.json.JsonObject;
 import org.eclipse.edc.api.model.ApiCoreSchema;
 
 @OpenAPIDefinition(
-        info = @Info(
-                description = "This represents the Federated Catalog API. It serves the cached Catalogs fetched from the data providers.",
-                title = "Federated Catalog API",
-                version = "v1"
-        )
-)
+        info = @Info(description = "This represents the Federated Catalog API. It serves the cached Catalogs fetched from the data providers.",
+                title = "Federated Catalog API", version = "v1"))
 @Tag(name = "Federated Catalog")
 public interface FederatedCatalogApi {
-
-    @Operation(
-            description = "Obtains all Catalog currently held by this cache instance",
+    @Operation(description = "Obtains all Catalog currently held by this cache instance",
             requestBody = @RequestBody(content = @Content(schema = @Schema(implementation = ApiCoreSchema.QuerySpecSchema.class))),
             parameters = @Parameter(name = "flatten", description = "Whether the resulting root catalog should be 'flattened' or contain a hierarchy of catalogs"),
             responses = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "A list of Catalog is returned, potentially empty",
-                            content = @Content(array = @ArraySchema(schema = @Schema(implementation = CatalogResponseSchema.class)))
-                    ),
+                    @ApiResponse(responseCode = "200", description = "A list of Catalog is returned, potentially empty",
+                            content = @Content(array = @ArraySchema(schema = @Schema(implementation = CatalogSchema.class)))),
                     @ApiResponse(responseCode = "500", description = "A Query could not be completed due to an internal error")
             }
+
     )
     JsonArray getCachedCatalog(JsonObject querySpec, boolean flatten);
+
+    @Schema(name = "Catalog", description = "DCAT catalog", example = CatalogSchema.CATALOG_EXAMPLE)
+    record CatalogSchema(
+    ) {
+        public static final String CATALOG_EXAMPLE = """
+                {
+                    "@id": "7df65569-8c59-4013-b1c0-fa14f6641bf2",
+                    "@type": "dcat:Catalog",
+                    "dcat:dataset": {
+                        "@id": "bcca61be-e82e-4da6-bfec-9716a56cef35",
+                        "@type": "dcat:Dataset",
+                        "odrl:hasPolicy": {
+                            "@id": "OGU0ZTMzMGMtODQ2ZS00ZWMxLThmOGQtNWQxNWM0NmI2NmY4:YmNjYTYxYmUtZTgyZS00ZGE2LWJmZWMtOTcxNmE1NmNlZjM1:NDY2ZTZhMmEtNjQ1Yy00ZGQ0LWFlZDktMjdjNGJkZTU4MDNj",
+                            "@type": "odrl:Set",
+                            "odrl:permission": {
+                                "odrl:target": "bcca61be-e82e-4da6-bfec-9716a56cef35",
+                                "odrl:action": {
+                                    "odrl:type": "http://www.w3.org/ns/odrl/2/use"
+                                },
+                                "odrl:constraint": {
+                                    "odrl:and": [
+                                        {
+                                            "odrl:leftOperand": "https://w3id.org/edc/v0.0.1/ns/inForceDate",
+                                            "odrl:operator": {
+                                                "@id": "odrl:gteq"
+                                            },
+                                            "odrl:rightOperand": "2023-07-07T07:19:58.585601395Z"
+                                        },
+                                        {
+                                            "odrl:leftOperand": "https://w3id.org/edc/v0.0.1/ns/inForceDate",
+                                            "odrl:operator": {
+                                                "@id": "odrl:lteq"
+                                            },
+                                            "odrl:rightOperand": "2023-07-12T07:19:58.585601395Z"
+                                        }
+                                    ]
+                                }
+                            },
+                            "odrl:prohibition": [],
+                            "odrl:obligation": []
+                        },
+                        "dcat:distribution": [
+                            {
+                                "@type": "dcat:Distribution",
+                                "dct:format": {
+                                    "@id": "HttpData"
+                                },
+                                "dcat:accessService": "5e839777-d93e-4785-8972-1005f51cf367"
+                            }
+                        ],
+                        "description": "description",
+                        "id": "bcca61be-e82e-4da6-bfec-9716a56cef35"
+                    },
+                    "dcat:service": {
+                        "@id": "5e839777-d93e-4785-8972-1005f51cf367",
+                        "@type": "dcat:DataService",
+                        "dct:terms": "connector",
+                        "dcat:endpointURL": "http://localhost:16806/protocol"
+                    },
+                    "dspace:participantId": "urn:connector:provider",
+                    "@context": {
+                        "@vocab": "https://w3id.org/edc/v0.0.1/ns/",
+                        "dct": "http://purl.org/dc/terms/",
+                        "edc": "https://w3id.org/edc/v0.0.1/ns/",
+                        "dcat": "http://www.w3.org/ns/dcat#",
+                        "odrl": "http://www.w3.org/ns/odrl/2/",
+                        "dspace": "https://w3id.org/dspace/2025/1/"
+                    }
+                }
+                """;
+    }
 }
+
